404b6071526dbb6ebb97a9a7c1312c19bbb2aa7e
Bheemalinga <kummarabheema@gmail.com>
2023-11-05 20:39:08 +0530

    Optimizations

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+  registerController,
+  loginController,
+  testController,
+} from "../controllers/authController.js";
+
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 // Creating the router object
 const router = express.Router();
@@ -7,5 +13,11 @@ const router = express.Router();
 // Routing
 // Register || POST
 router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
 
 export default router;
